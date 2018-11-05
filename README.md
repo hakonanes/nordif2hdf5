bfd114809c1294886e6e834aad911beb9c53fe10
Håkon Wiik Ånes <hwaanes@gmail.com>
2018-11-05 17:04:28 +0100

    Upload stuff

---
+++ README.md
@@ -0,0 +1,10 @@
+# nordif2hdf5
+
+Convert NORDIF DAT-file with Kikuchi diffraction patterns to HyperSpy HDF5
+format.
+
+Running the script:
+
+  * Install necessary libraries [HyperSpy](https://github.com/hyperspy/hyperspy/) and [NumPy](http://www.numpy.org/)) into your [Conda](https://www.anaconda.com/download/) environment or virtualenv.
+  * Change file path and name and grid dimension and pattern size in file
+  * Run file
---
+++ nordif2hdf5.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Convert NORDIF DAT-file with Kikuchi diffraction patterns to HyperSpy HDF5
+format.
+
+Assumes you can have your full dataset in memory! Will look at a smarter
+solution in the future.
+
+Created by Håkon W. Ånes (hakon.w.anes@ntnu.no)
+2018-10-30
+"""
+
+import hyperspy.api as hs
+import numpy as np
+
+
+# Set file path and file name
+datadir = '/path/to/file'
+fname = 'Pattern'
+
+# Set grid dimensions and pattern size (in px). Will take this from
+# Settings.txt in the near future with regular expressions
+scanx = 409
+scany = 409
+dp_width = 120 # Acquisition px
+dp_height = 120
+
+dp_size = dp_width * dp_height  # 1 byte per pixel
+dat_size = scanx * scany * dp_size
+
+dat = np.zeros((scany, scanx, dp_height, dp_width), dtype='uint8')
+
+with open(dir + fname + '.dat', mode='rb') as f:
+    f.seek(-dat_size, 2)  # 2: from end of file
+    for y in range(scany):
+        for x in range(scanx):
+            img = f.read(dp_size)
+            img = np.fromstring(img, dtype='uint8')  # creates 1-D array
+            img.shape = (dp_height, dp_width)  # make 2-D array
+            dat[y, x, :, :] = img
+
+s = hs.signals.Signal2D(dat)
+
+s.save(dir + fname + '.hdf5')
